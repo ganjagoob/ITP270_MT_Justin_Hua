c7fa833ca3e71c4e91a95857ae0aec8a93249d8d
ganjagoob <justinhuuua@gmail.com>
2023-03-09 20:04:26 -0500

    finished my MT

---
+++ MT_Justin_Hua.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+read -p "Enter 'google.com' to ping, anythig else is invalid: " hostname
+
+#The input validation
+if [ "$hostname" != "google.com" ]; then
+  echo "The input must be google.com, anything else is invalid"
+  exit 1
+fi
+
+#  
+ping -c 5 "$hostname" > ping_results.txt
+
+# Print a message indicating that the ping is complete
+echo "Success! Results added to ping_results.txt."
+
